89d3d904b2afd2b0f4d55a81159811dafe08156a
Luís Paulo <luisjnk@gmail.com>
2025-02-05 17:31:27 +0000

    Cleaned up the project

--- src/components/CurrencyInput/CurrencyInput.tsx
+++ src/components/CurrencyInput/CurrencyInput.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef, useState } from 'react';
+import React, { useState } from 'react';
 import './CurrencyInput.css';
 import {
   formatToLocaleString,
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,4 @@
 export * from './CurrencyConverter';
-export * from './Header';
 export * from './Loading';
 export * from './RateList';
 export * from './Error';
